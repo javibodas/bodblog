0472c1d6c35139c96053cecf735978e28f845d9b
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2020-09-19 13:34:26 +0200

    Support for delete post comments. Modify colors and structure of comment. Add pointer cursor when hover in postslist.

--- src/components/PostComment/index.js
+++ src/components/PostComment/index.js
@@ -1,25 +1,45 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function Comment(props){
 
     return(
     <>
         <section className='comment'>
-            <img className='img-username-comment' src={ props.img ? props.img : "/images/default-user-comment-img.png"}></img>
-            <span className='username-comment'>{props.name}</span>
-            <date className='date-comment'>{props.date}</date>
+            <div className='comment-header'>
+                <div className='comment-info'>
+                    <img className='img-username-comment' src={ props.img ? props.img : "/images/default-user-comment-img.png"}></img>
+                    <span className='username-comment'>{props.name}</span>
+                    <date className='date-comment'>{props.date}</date>
+                </div>
+                <span className='delete-icon'><FontAwesomeIcon icon={faTrashAlt} onClick={() => props.removeCommentAction(props.id)}/></span>
+            </div>
             <p className='text-comment'>{props.text}</p>
         </section>
         <style jsx>{`
         
            .comment{
-            padding: 0.5rem 0 0.5rem 0;
-            display: flex;
-            flex-direction: column;
+                padding: 1rem 1rem 1rem 1rem;
+                display: flex;
+                flex-direction: column;
+                border-radius: 10px;
+                background-color: #faf8f7;
+            }
+
+            .comment-header{
+                display: flex;
+                flex-direction: row;
+            }
+            .comment-info{
+                display: flex;
+                flex-direction: column;
+                width: 95%;
             }
             
             .date-comment{
-                font-size: 12px;
+                color:#8f8f8f;
+			    font-size:0.725rem
             }
             
             .username-comment{
@@ -34,6 +54,15 @@ export default function Comment(props){
             
             .text-comment{
                 margin-top: 0.5rem;
+            }
+
+            .delete-icon > svg{
+                opacity: 0.5;
+            }
+            .delete-icon > svg:hover{
+                cursor: pointer;
+                color: red;
+                opacity: 1;
             }
         
         `}</style>
--- src/components/PostComments/index.js
+++ src/components/PostComments/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Comment from 'components/PostComment'
 import addPostComment from 'services/comments/addPostComment';
+import deletePostComment from 'services/comments/deletePostComment';
 import useComments from 'hooks/useComments';
 
 export default function PostComments(props){
@@ -37,6 +38,14 @@ export default function PostComments(props){
         })
         .catch((error) => console.log('Error while adding comment to post ' + props.postid + ' - ' + error))
     }
+
+    const removeComment = function(id){
+        deletePostComment(id)
+        .then((comment) => {
+            setComment(comment)
+        })
+        .catch((error) => { alert('Something wrong occurred when delete comment ' + id)})
+    }
    
     return(
         <>    
@@ -44,10 +53,10 @@ export default function PostComments(props){
                 <form className='form-add-comment'>
                     <input className='form-control' type='text' placeholder='Nombre' id='form-username-comment'></input>
                     <textarea className='form-control' type='text' placeholder='Comentario...' id='form-text-comment'></textarea>
-                    <button className='btn btn-primary' onClick={clickSendComment}>Enviar</button>
+                    <button className='btn' onClick={clickSendComment}>Enviar</button>
                 </form>
                 <div className='list-comments'>    
-                    { comments.map((comment) => <Comment name={comment.username} date={comment.publicationDate} text={comment.comment}/>) }
+                    { comments.map((comment) => <Comment id={comment.id} name={comment.username} date={comment.publicationDate} text={comment.comment} removeCommentAction={removeComment}/>) }
                 </div>
             </section>
             <style jsx>{`
@@ -71,12 +80,19 @@ export default function PostComments(props){
                 
                 .form-add-comment > button{
                    width: 100px;
-                   background-color: var(--main-color)
+                   color: white;
+                   background-color: var(--main-color);
                 }
 
                 .form-add-comment > button:hover{
-                    width: 100px;
-                    background-color: var(--main-dark-color)
+                    background-color: var(--main-dark-color);
+                    color: var(--links-hover-color);
+                    border: solid 1px var(--links-hover-color);
+                 }
+
+                 .form-add-comment > input:focus, .form-add-comment > textarea:focus{
+                    border-color: var(--links-color)!important;
+                    box-shadow: 0 0 0 0.05rem var(--links-color)!important;
                  }
                 
                 @media screen and (min-width: 480px) {
